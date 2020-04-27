a48f151beb7260ed9238cfb3fa57c1d776f61d9d
ngsylar <ngsylar@gmail.com>
2020-04-27 19:55:50 -0300

    Comments.

--- calculator.rb
+++ calculator.rb
@@ -10,38 +10,39 @@ expression = "   4*23- 2+          2*57-4 / 70         "
 #     return
 # end
 
-temp_array = expression.split
+temp_array = expression.split       # separa a string por espaços e guarda num array
 expression.clear
 for array_slice in temp_array
-    expression << array_slice
+    expression << array_slice       # une os pedaços na string
 end
 
 delimiters = [' ', '+', '-', '*', '/']
-temp_array = expression.split('')
+temp_array = expression.split('')   # separa a string por digitos e guarda num array
 expression = Array.new
 exp_last_number = false
 p temp_array
-for token in temp_array
+
+for token in temp_array                 # para cada digito da expressao
     op_flag = false
     for operator in delimiters
-        if token == operator
+        if token == operator            # compara digito com cada operador
             op_flag = true
             exp_last_number = false
             break
         end
     end
     
-    if !op_flag
-        if exp_last_number
-            expression.last << token
-        else
-            expression.push(token)
+    if !op_flag                         # se digito nao é operador
+        if exp_last_number              # se ultimo digito é numero
+            expression.last << token    # junta numero atual ao anterior
+        else                            # se ultimo digito é operador
+            expression.push(token)      # recoloca numero na expressao como elemento distinto do anterior
             exp_last_number = true
         end
-    elsif expression.empty?
+    elsif expression.empty?             # se primeiro digito da expressao é um operador, erro
         p "error"
-    else
-        expression.push(token)
+    else                                # se digito é operador 
+        expression.push(token)          # recoloca na expressao como elemento distinto
         exp_last_number = false
     end
 end
