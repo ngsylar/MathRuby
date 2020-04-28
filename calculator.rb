cabc8b57acae5234cfd000ec5e1c883853df4c35
ngsylar <ngsylar@gmail.com>
2020-04-27 21:44:42 -0300

    Parsing done.

--- calculator.rb
+++ calculator.rb
@@ -4,7 +4,11 @@ puts "Example: \"4*2-(2+2)\" (without the quotes)"
 puts ""
 print "x = "
 
-expression = "   4*23- 2+          2*57-4 / 70      +*++8   "
+# expression = "   4*23- 2+          2*1(1+(57-4))1 / 2     +*++8   "
+# expression = "   4*23- 2+          2*((57-4+))1 / 2     +*++8   "
+# expression = "   4*23- 2+          2*((57-4))1 / 2     +*++8   "
+# expression = "   4*23- 2+          2*((57-4)) / 2     +*++8   "
+expression = "   4*23- 2+          2*((57-4)) / 2        "
 # expression = gets.chomp
 # if expression.downcase == "exit"
 #     return
@@ -16,35 +20,62 @@ for array_slice in temp_array
     expression << array_slice       # une os pedaços na string
 end
 
-delimiters = [' ', '+', '-', '*', '/']
+delimiters = ['+', '-', '*', '/']
 temp_array = expression.split('')   # separa a string por digitos e guarda num array
+# p temp_array
 expression = Array.new
-exp_last_number = false
-p temp_array
+last_tk_numbr = false
+last_tk_prths = false
+parsing_error = false
 
 for token in temp_array                 # para cada digito da expressao
     op_flag = false
     for operator in delimiters
-        if token == operator            # compara digito com cada operador
+        if token == operator            # compara digito atual com cada operador
             op_flag = true
+            last_tk_prths = false
             break
         end
     end
     
-    if !op_flag                         # se digito nao é operador
-        if exp_last_number              # se ultimo digito é numero
+    if !op_flag                         # se digito atual nao é operador
+        if token == '('                 # se digito atual é abre parentesis
+            if last_tk_numbr            # se digito anterior for numero, erro
+                print token
+                p " error"
+                break
+            end
+            expression.push(token)      # recoloca parentesis como elemento individual
+        elsif token == ')'                              # se digito atual é fecha parentesis
+            if expression.empty? or !last_tk_numbr      # se digito anterior nao for numero,erro
+                print token
+                p " error"
+                break
+            end
+            expression.push(token)      # recoloca parentesis como elemento individual
+            last_tk_prths = true
+
+        elsif last_tk_numbr             # se digito atual é numero
+            if last_tk_prths            # se digito anterior é fecha parentesis, erro
+                print token
+                p " error"
+                break
+            end                         # se digito anterior é numero
             expression.last << token    # junta numero atual ao anterior
-        else                            # se ultimo digito é operador
+        else                            # se digito anterior é operador
             expression.push(token)      # recoloca numero na expressao como elemento distinto do anterior
-            exp_last_number = true
+            last_tk_numbr = true
         end
+
     # se primeiro digito da expressao é um operador ou se ambos digito atual e anterior são operadores, erro
-    elsif expression.empty? or !exp_last_number
+    elsif expression.empty? or !last_tk_numbr
         print token
         p " error"
-    else                                # se digito é operador 
+        break
+
+    else                                # se digito atual é operador 
         expression.push(token)          # recoloca na expressao como elemento distinto
-        exp_last_number = false
+        last_tk_numbr = false
     end
 end
 p expression
