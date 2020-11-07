cb537bff2d68049b0ab7d5c64fe97710708c78c3
ngsylar <ngsylar@gmail.com>
2020-11-07 16:05:50 -0300

    Parte 1 completa

--- homework_3/features/step_definitions/movie_steps.rb
+++ homework_3/features/step_definitions/movie_steps.rb
@@ -4,6 +4,12 @@ Given "the following movies exist:" do |table|
     end
 end
 
+When "I check all ratings" do
+    Movie.all_ratings.each do |rating|
+        check("ratings[#{rating}]")
+    end
+end
+
 When /^I check the following ratings: (.*)$/ do |ratings|
     ratings.split(/,[ ]*/).each do |rating|
         check("ratings[#{rating}]")
@@ -13,5 +19,23 @@ end
 When /^I uncheck the following ratings: (.*)$/ do |ratings|
     ratings.split(/,[ ]*/).each do |rating|
         uncheck("ratings[#{rating}]")
+    end
+end
+
+Then "I should see all of the movies" do
+    titles = Movie.all.map { |movie| movie.title }
+
+    if page.respond_to? :should
+        titles.each { |title| page.should have_content(title) }
+    else
+        titles.each { |title| assert page.has_content?(title) }
+    end
+end
+
+Then /^I should see "([^"]*)" before "([^"]*)"$/ do |title1, title2|
+    if page.respond_to? :should
+        page.should have_content(/#{title1}(.*)#{title2}/m)
+    else
+        assert page.has_content?(/#{title1}(.*)#{title2}/m)
     end
 end
